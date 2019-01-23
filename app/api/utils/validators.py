eb68aa091a13740ac7113fd2ba61eaf5b77c7a69
bencyn <bensonnjung39@gmail.com>
2019-01-24 02:29:53 +0300

    [feature #163412623] refactor code

--- app/api/utils/validators.py
+++ app/api/utils/validators.py
@@ -11,11 +11,8 @@ class Validators():
 
     def validation_message(self,field):
         """ this method generates an empty error message for empty fields"""
-        
-        return make_response(jsonify({
-                "status": 400,
-                "error": "{} cannot be empty".format(field)
-            })), 400
+        error = "{} cannot be empty".format(field)
+        return self.get_response(error)
     
 
     def _validate(self,input):
@@ -24,35 +21,26 @@ class Validators():
         for key,value in input.items():
             if value is not None:
                 if not value.strip():
-                    return self.validate_whitespaces(key)
+                    error ="{} cannot be empty".format(key)
+                    return self.get_response(error)
                 if key == "email":
+
                     return self.validate_email(value)
             else:
-               return self.validate_required_field(key)
+               error ="{} field required".format(key)
+               return self.get_response(error)
 
-    def validate_missing_data(self):
-        """ validates missing json object"""
-
-        return jsonify({'status': 400,
-                        'error': "Bad request: attach missing fields"}),400
-
-    def validate_whitespaces(self,key):
-        """ return reponse for field with whitespaces"""
-        return jsonify({
-                        "status": 400,
-                        "error": "{} cannot be empty".format(key)
-                    }), 400
 
     def validate_email(self,value):
         """checks for a valid email"""
         if  not re.match(r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$',
                                         value):
-            return jsonify({'status': 400,
-                        'error': "email provided is invalid"}),400
+            error = "email provided is invalid"
+            return self.get_response(error)
                         
-    def validate_required_field(self,key):
-        """returns a response for required fields"""
+    def get_response(self,error):
+        """returns an error response based on the error values"""
 
         return jsonify({'status': 400,
-                                 "error": "{} field required".format(key)
-                            }),400
+                        "error": error
+                    }),400
