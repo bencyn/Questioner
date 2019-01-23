691b6cd8dc0e64f8d61f71f03c06b3a0efc45055
bencyn <bensonnjung39@gmail.com>
2019-01-24 02:20:54 +0300

    [feature #163412623] add more functionality

--- app/api/v2/models/meetup_model.py
+++ app/api/v2/models/meetup_model.py
@@ -19,14 +19,29 @@ class Meetup(BaseModel):
         self.tags= kwargs['tags']
         self.user_id= kwargs['user_id']
         
+        user = self.get_by_key("users","id",self.user_id)
 
-        sql = """ INSERT INTO meetups (happening_on,location,images,topic,tags,user_id)
-                VALUES('{}','{}','{}','{}','{}','{}') RETURNING meetups.id;""".format(self.happening_on,self.location,self.images,self.topic,
-                self.tags,self.user_id)
+        if user:
+            admin = user[0]["is_admin"] 
+            if admin == "1":
+                sql = """ INSERT INTO meetups (happening_on,location,images,topic,tags,user_id)
+                        VALUES('{}','{}','{}','{}','{}','{}') RETURNING meetups.id;""".format(self.happening_on,self.location,self.images,self.topic,
+                         self.tags,self.user_id)
 
-        save_meetup=self.save_data(sql)
-        meetup = self.get_by_key("meetups","id",save_meetup["id"])
-        return meetup
+                save_meetup=self.save_data(sql)
+                meetup = self.get_by_key("meetups","id",save_meetup["id"])
+
+                return jsonify({ 
+                    "status": 201,
+                    "data":meetup,
+                    "message":"meetup record created successfully",
+                }), 201
+            else:
+                return jsonify({'msg': 'user is not an admin' }), 401
+        else:
+            return jsonify({'msg': 'user does not exist' }), 404
+
+        
    
     def login_user(self):
         """"""
