0d8b11852c12ba6a73985caed27964ca7193bde5
bencyn <bensonnjung39@gmail.com>
2019-02-07 01:18:14 +0300

    [feature#163740304] add logout function in js

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -1,11 +1,13 @@
-import { logout } from './helper.js';
+import { logout,append_logout } from './helper.js';
 
-window.onload = function () {
-    // append_logout()
-    var logout_link = document.querySelector('.logout');
+
+append_logout()
+var logout_link = document.querySelector('.logout');
+if(logout_link){
     logout_link.addEventListener('click', logout)
-    
 }
+
+
 
 
 
