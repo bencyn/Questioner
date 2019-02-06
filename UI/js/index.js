0954b2b715da0d7e937d86dcc8176ca04657c8ef
bencyn <bensonnjung39@gmail.com>
2019-02-07 01:16:03 +0300

    [feature#163740304] refactor code

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,14 +1,10 @@
-import { logout, append_logout } from './helper.js';
+import { logout,append_logout } from './helper.js';
 
-window.onload = function () {
-    append_logout
-    var logout_link = document.querySelector('.logout');
-    if(logout_link){
-        logout_link.addEventListener('click', logout)
-    }
-
+append_logout()
+var logout_link = document.querySelector('.logout');
+if(logout_link){
+    logout_link.addEventListener('click', logout)
 }
-
 
 
 
