5d507ef3159c2809c40d7601c1e4db38b62761a5
bencyn <bensonnjung39@gmail.com>
2019-02-09 20:34:42 +0300

    [start #163436406] adjust queries

--- app/api/v2/models/base_model.py
+++ app/api/v2/models/base_model.py
@@ -34,17 +34,19 @@ class BaseModel:
        
         query = "SELECT * FROM {} WHERE {}='{}'".format(table, field, data)
         conn.cursor.execute(query)
+        print(query)
+        conn.connection.commit()
         if conn.cursor.fetchone():
             return True
         else:
             return False
 
-    def check_if_vote_exists(self,data,data2,status):
+    def check_if_vote_exists(self,user,question,status):
         """ checks if a vote is present"""
         if status == "upvote":
-            query = "SELECT * FROM votes WHERE user_id='{}' AND question_id='{}' AND number=1".format(data,data2)
+            query = "SELECT * FROM votes WHERE user_id='{}' AND question_id='{}' AND upvotes=1".format(user,question)
         else:
-            query = "SELECT * FROM votes WHERE user_id='{}' AND question_id='{}' AND number=-1".format(data,data2)
+            query = "SELECT * FROM votes WHERE user_id='{}' AND question_id='{}' AND downvotes=1".format(user,question)
         conn.cursor.execute(query)
         if conn.cursor.fetchone():
             return True
@@ -90,19 +92,28 @@ class BaseModel:
             query="""SELECT * FROM {} WHERE happening_on > '{}' """.format(table,current_date)
         else:
             query="""SELECT * FROM {} """.format(table)
+        print(query)
         conn.cursor.execute(query)
         data = conn.cursor.fetchall()
+        conn.connection.commit()
         result = []
         for row in data:
             result.append(dict(row))
         return result
 
-    def get_by_key(self,table,field,value):
-        """ get records  by id"""
-        query = """SELECT * FROM %s WHERE %s='%s';"""%(table,field,value)
+    def get_by_query(self,status):
+        """fetch data by query passed"""
+        if(status=="posted"):
+            query=''
+        if(status =="feeds"):
+            query=''
+        if(status=="commented"):
+            query=''
+            
         try:
             conn.cursor.execute(query)
             data= conn.cursor.fetchall()  
+            conn.connection.commit()
             if not data:
                 return False
             else:
@@ -114,7 +125,47 @@ class BaseModel:
         except ValueError as e:
             message = {'message': '{}'.format(e)}
             return jsonify({"status": 400,"error":message}), 400
-    
+
+    def get_by_key(self,table,field,value):
+        """ get records  by id"""
+        query = """SELECT * FROM %s WHERE %s='%s' ORDER BY id DESC;"""%(table,field,value)
+        
+        try:
+            conn.cursor.execute(query)
+            data= conn.cursor.fetchall()  
+            conn.connection.commit()
+            if not data:
+                return False
+            else:
+                result = []
+                for row in data:
+                    result.append(dict(row))
+                return result
+
+        except ValueError as e:
+            message = {'message': '{}'.format(e)}
+            return jsonify({"status": 400,"error":message}), 400
+    def get_with_user(self,table,cast,field,value):
+        """ get records  by id"""
+        query = """SELECT *,{x}.id as {x}_id FROM %s RIGHT JOIN users ON users.id = CAST (%s AS INTEGER) WHERE %s='%s' ORDER BY {x}.id DESC;""".format(x=table)%(table,cast,field,value)
+        
+        try:
+            conn.cursor.execute(query)
+            data= conn.cursor.fetchall() 
+            print(query) 
+            conn.connection.commit()
+            if not data:
+                return False
+            else:
+                result = []
+                for row in data:
+                    result.append(dict(row))
+                return result
+
+        except ValueError as e:
+            message = {'message': '{}'.format(e)}
+            return jsonify({"status": 400,"error":message}), 400
+
     def delete_by_key(self,table,field,value):
         """ delete record by id"""
         query = "DELETE FROM {} WHERE {}={}".format(table,field,value)
@@ -125,7 +176,7 @@ class BaseModel:
         except ValueError as e:
             message = {'message': '{}'.format(e)}
             return jsonify({"status": 400,"error":message}), 400
-       
+   
 
     def execute_query(self,query):
         conn.cursor.execute(query)
